192e1fc6e7220e1b529c4afc16ec1a6024fdfa10
zalogon12 <88121194+zalogon12@users.noreply.github.com>
2023-01-15 19:47:23 -0300

    Primer commit al que se le va a crear las bases de la pagina

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//define('URL', 'http://localhost/mvc/');
+define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/enzodg/');
+
+//conexion a la base de datos
+define('HOST', 'enzodg');
+define('PORT', '3306');
+define('DB', '');
+define('USER', 'root');
+define('PASSWORD', "");
+define('CHARSET', 'utf8mb4');
