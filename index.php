b8c2b1466c3716bce823dc22d37f5f10458da9e2
zalogon12 <88121194+zalogon12@users.noreply.github.com>
2023-01-23 18:14:29 -0300

    mas contenido 2.0

--- index.php
+++ index.php
@@ -11,15 +11,30 @@
 <body>
     <div class="cont-pagina">
 <?php require_once 'views/header.php';?>
+
 <div class="intro">
     <h1>Enzo Romano - Diseño Gráfico</h1>
     <p>Bienvenido a mi sitio web, aquí podrás encontrar una selección de mis proyectos de diseño gráfico.</p>
     <p>Soy un diseñador gráfico con experiencia en el campo y me apasiona compartir mi trabajo.</p>
     <p>¡Espero que disfrutes de lo que tengo que ofrecer!</p>
+    <h1>Mi experiencia en Diseño</h1>
+    <p>En mi experiencia, he trabajado con herramientas de edición de imagen como Photoshop y Illustrator.</p>
+  <p>He aprendido a manejar los diferentes recursos que ofrecen para editar y crear imágenes.</p>
+  <p>Además, he trabajado con el programa de edición de video Sony Vegas para editar y crear videos con un alto nivel de calidad.</p>
+  <p>A través de la práctica, he logrado desarrollar habilidades en la retoque de fotografías, creación de gráficos vectoriales, diseño de elementos para diferentes proyectos y edición de videos.</p>
+ 
+  <div class="icon-tecn"> 
+    <img src="public/img/itor.png" alt=""> 
+    <img src="public/img/psp.png" alt=""> 
+    <img src="public/img/sv.png" alt="">
+    </div>
 </div>
+<div class="tutul-foto"></div>
+
 <div class="cont-tre">
 
 </div>
 </div>
+
 </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,7 +1,21 @@
 <div class="cont-hi">
-
-    
     <div class="menu">
-    
+    <div class="cont-br">
+        <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Diseños</a></li>
+</ul></div>
+       <div class="redes">
+<a href="#"><img src="public/img/waap.png" alt=""></a>
+<a href="#"><img src="public/img/igma.png" alt=""></a>
+<a href="#"><img src="public/img/linke.png" alt=""></a>
+       
+</div>
+<div class="logot">
+
+<a href="#"><img src="public/img/logo.png" alt=""></a>
+       
     </div>
+</div>
 </div>
